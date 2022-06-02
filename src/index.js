446d65fc4fa4adbd7cd29c47bf9a3ab7a0e23cff
Chris <christoment@gmail.com>
2022-06-02 15:20:05 +1000

    Add more logging message

--- src/index.js
+++ src/index.js
@@ -8,14 +8,28 @@ dotenv.config();
 const PORT = process.env.PORT ?? 4500;
 const DELAY = process.env.DELAY ?? 5000;
 const mode = process.env.MODE ?? 'producer';
+const ATTEMPT_HEADER = 'x-attempt';
 
 if (mode === 'consumer') {
+    let failedCount = 0;
+    let attemptCount = 0;
+    
     const fetcher = () => new Promise((resolve) => {
         setTimeout(async () => {
             try {
-                const result = await axios.get(`http://localhost:${PORT}`);
+                attemptCount++;
+                
+                if (failedCount > 0) {
+                    console.log(`Request #${attemptCount} initiated. Retry attempt #${failedCount}`);
+                } else {
+                    console.log(`Request #${attemptCount} initiated.`);
+                }
+
+                const result = await axios.get(`http://localhost:${PORT}`, { headers: { [ATTEMPT_HEADER]: attemptCount.toString() }});
+                failedCount = 0;
                 resolve(result.data);
             } catch (e) {
+                failedCount++;
                 console.error('Failed to fetch.', e.message);
                 resolve(null);
             }
@@ -30,8 +44,10 @@ if (mode === 'consumer') {
     }
 } else {
     http.createServer((req, res) => {
-        res.writeHead(200);
+        const requestIndex = req.headers[ATTEMPT_HEADER] ?? 'N/A';
+        console.log(`Request #${requestIndex} received.`);
         const randomNumber = v4();
+        res.writeHead(200);
         res.write(`Your random guid is ${randomNumber} - generated at ${new Date().toISOString()}`);
         res.end();
     }).listen(PORT);
